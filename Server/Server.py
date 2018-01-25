7b8d8d2f1961bad03d90514c4e18143dcc09f56b
CodePirateArg <35698773+CodePirateArg@users.noreply.github.com>
2018-01-25 16:13:02 -0500

    Added new beta implementation of the server

--- Server/Server.py
+++ Server/Server.py
@@ -1,5 +1,7 @@
 import sys
 import socket
+import threading
+import asyncio
 
 
 class ServerListener:
@@ -12,30 +14,37 @@ class ServerListener:
         self.users = {}
         self.connections = {}
 
+    def receive_data(self, connection):
+        while True:
+            data = connection.recv(1024)
+            print(sys.stderr, 'received "%s"' % data)
+            decoded_data = data.decode('ascii')
+            if decoded_data[:5] == "User:":
+                temp_username = decoded_data[5:]
+                self.users[temp_username] = [0, 0]
+                print("User: %s has been added to the game" % temp_username)
+                connection.sendall(("Welcome %s" % temp_username).encode())
+            else:
+                # print(sys.stderr, 'no more data from', self.client_address)
+                break
+
+
     def start_listening(self):
         self.sock.listen(1)
         while True:
             print("Waiting for a connection")
-            self.connection, self.client_address = self.sock.accept()
+            connection, client_address = self.sock.accept()
+            print(connection)
             try:
-                print(sys.stderr, 'connection from', self.client_address)
+                print(sys.stderr, 'connection from', client_address)
                 # Receive the data (either a username or a direction)
-                while True:
-                    data = self.connection.recv(1024)
-                    print(sys.stderr, 'received "%s"' % data)
-                    decoded_data = data.decode('ascii')
-                    if decoded_data[:5] == "User:":
-                        temp_username = decoded_data[5:]
-                        self.users[temp_username] = [0, 0]
-                        print("User: %s has been added to the game" % temp_username)
-                        self.connection.sendall(("Welcome %s" % temp_username).encode())
-                    else:
-                        #print(sys.stderr, 'no more data from', self.client_address)
-                        break
+                thread = threading.Thread(target=self.receive_data(connection))
+                thread.start()
 
             finally:
                 # Clean up the connection
-                self.connection.close()
+                print("connection closed")
+                connection.close()
 
     #def register_user(self, username):
 
@@ -43,6 +52,17 @@ class ServerListener:
 def main():
     server = ServerListener('', 10000)
     server.start_listening()
+    #thread = threading.Thread(target=server.start_listening(), args=())
+    #thread2 = threading.Thread(target=server.start_listening(), args=())
+    #thread.daemon = True
+    #thread2.daemon = True
+    #thread.start()
+    #thread2.start()
+    ##ioloop = asyncio.get_event_loop()
+    ##tasks = [ioloop.create_task(server.start_listening())]
+    ##wait_tasks = asyncio.wait(tasks)
+    ##ioloop.run_until_complete(wait_tasks)
+    ##ioloop.close()
 
 
 if __name__ == "__main__":
